321ff8be4d9f7facf3a36930c1b7dde201e4efe7
Igor Chernov <igorek.chernov007@gmail.com>
2022-07-07 23:17:34 +0300

    Створив  модель Article. Додано 3 поля і конструктор

---
+++ Newbisoft/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿namespace Newbisoft.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
+
+        public Article()
+        {
+
+        }
+    }
+}
