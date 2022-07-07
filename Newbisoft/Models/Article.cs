2d6465a9af126fe441615f5b216423cffa21c9d0
Heorhii <georgiy.y.pitsyk@gmail.com>
2022-07-08 00:12:32 +0300

    Revert "Додав поле Comments та NULL значення для полів"
    This reverts commit 6e8b93f891dde9088b8a1b385c2f85c384775c10.

--- Newbisoft/Models/Article.cs
+++ Newbisoft/Models/Article.cs
@@ -3,9 +3,8 @@
     public class Article
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Name { get; set; }
-        public string? Comments { get; set; }
+        public string Title { get; set; }
+        public string Name { get; set; }
 
         public Article()
         {
