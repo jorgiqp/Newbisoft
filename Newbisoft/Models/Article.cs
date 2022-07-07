6e8b93f891dde9088b8a1b385c2f85c384775c10
Igor Chernov <igorek.chernov007@gmail.com>
2022-07-08 00:04:20 +0300

    Додав поле Comments та NULL значення для полів

--- Newbisoft/Models/Article.cs
+++ Newbisoft/Models/Article.cs
@@ -3,8 +3,9 @@
     public class Article
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Name { get; set; }
+        public string? Title { get; set; }
+        public string? Name { get; set; }
+        public string? Comments { get; set; }
 
         public Article()
         {
