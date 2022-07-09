d0edcfbfe07208342ac25986a249fd6f0a1c5195
Igor <igorek.chernov007@gmail.com>
2022-07-09 22:20:35 +0300

    Додано у барі кнопку Edit. Відредаговано зовнішній вигляд бланку Edit

--- Newbisoft/Controllers/HomeController.cs
+++ Newbisoft/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace Newbisoft.Controllers
         {
             return View();
         }
+        public IActionResult Edit()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
